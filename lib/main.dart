c0d79d309d6da8200e06fda1920dca0dc4113ad1
AbdrahumanFikry <op.op25@yahoo.com>
2020-04-08 15:34:57 +0200

    Abdrahuman

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,12 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:senior/auth/loginScreen.dart';
+import 'package:senior/forceField/forceFieldNavigator.dart';
 import 'package:senior/providers/authenticationProvider.dart';
 import 'package:senior/providers/location.dart';
-import 'package:senior/senior/selectType.dart';
+import 'package:senior/sells/sellsNavigator.dart';
+import 'package:senior/senior/tabBarForceField.dart';
+import 'package:senior/senior/tabBarSells.dart';
 
 main() {
   runApp(
@@ -19,6 +22,32 @@ main() {
 }
 
 class MyApp extends StatelessWidget {
+  Widget navigator(String type, BuildContext context) {
+    Widget temp;
+    switch (Provider.of<Auth>(context, listen: false).type) {
+      case 'driver':
+        temp = SellsNavigator(
+          isDriver: true,
+        );
+        break;
+      case 'salles_man':
+        temp = SellsNavigator(
+          isDriver: false,
+        );
+        break;
+      case 'filed_force_man':
+        temp = ForceFieldNavigator();
+        break;
+      case 'sales_senior':
+        temp = TabBarScreenSells();
+        break;
+      case 'field_force_senior':
+        temp = TabBarForceFieldScreen();
+        break;
+    }
+    return temp;
+  }
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations(
@@ -36,29 +65,39 @@ class MyApp extends StatelessWidget {
           create: (context) => Auth(),
         ),
       ],
-      child: MaterialApp(
-        showSemanticsDebugger: false,
-        localizationsDelegates: [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          EasyLocalization.of(context).delegate,
-        ],
-        supportedLocales: EasyLocalization.of(context).supportedLocales,
-        locale: EasyLocalization.of(context).locale,
-        theme: ThemeData(
-          iconTheme: IconThemeData(
-            size: 16,
-          ),
-          textTheme: TextTheme(
-            subhead: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 15,
+      child: Consumer<Auth>(
+        builder: (context, auth, _) => MaterialApp(
+          showSemanticsDebugger: false,
+          localizationsDelegates: [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            EasyLocalization.of(context).delegate,
+          ],
+          supportedLocales: EasyLocalization.of(context).supportedLocales,
+          locale: EasyLocalization.of(context).locale,
+          theme: ThemeData(
+            iconTheme: IconThemeData(
+              size: 16,
             ),
+            textTheme: TextTheme(
+              subhead: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
+            scaffoldBackgroundColor: Colors.white,
           ),
-          scaffoldBackgroundColor: Colors.white,
+          debugShowCheckedModeBanner: false,
+          home: auth.isAuth
+              ? navigator(auth.type, context)
+              : FutureBuilder(
+                  future: auth.tryAutoLogin(),
+                  builder: (ctx, snapShot) =>
+                      snapShot.connectionState == ConnectionState.waiting
+                          ? navigator(auth.type, context)
+                          : LoginScreen(),
+                ),
         ),
-        debugShowCheckedModeBanner: false,
-        home: LoginScreen(),
       ),
     );
   }
