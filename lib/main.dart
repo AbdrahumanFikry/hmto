9194fac5fd4d0f3cf9d34874144f832261359e80
MohammedGhazy <me6636366@gmail.com>
2020-04-14 12:50:11 -0700

    edit ui

--- lib/main.dart
+++ lib/main.dart
@@ -122,7 +122,7 @@ class MyApp extends StatelessWidget {
                                 child: CircularProgressIndicator(),
                               ),
                             )
-                          :Store(),
+                          :LoginScreen(),
                 ),
         ),
       ),
