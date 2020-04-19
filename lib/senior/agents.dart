be1f45bb7bb73597bc369f69ae28b2cdd6a6a03c
AbdrahumanFikry <op.op25@yahoo.com>
2020-04-19 11:11:09 +0200

    commit

--- lib/senior/agents.dart
+++ lib/senior/agents.dart
@@ -11,14 +11,15 @@ class Agents extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder(
-        future:  Provider.of<FieldForceData>(context, listen: false).fetchAgents(),
-          builder: (context,dataSnapShot){
+          future:
+              Provider.of<FieldForceData>(context, listen: false).fetchAgents(),
+          builder: (context, dataSnapShot) {
             if (dataSnapShot.connectionState == ConnectionState.waiting) {
               return Center(
                 child: CircularProgressIndicator(),
               );
-            }else{
-              if(dataSnapShot.hasError){
+            } else {
+              if (dataSnapShot.hasError) {
                 Center(
                   child: Text(
                     tr('extra.check'),
@@ -29,110 +30,125 @@ class Agents extends StatelessWidget {
                   ),
                 );
               }
-              return Consumer<FieldForceData>(builder: (context, data, child){
+              return Consumer<FieldForceData>(builder: (context, data, child) {
                 return data.agentsModel.data.length == null
-                    ?Center(
-                  child: Text(
-                    tr('extra.noTarget'),
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      fontSize: 18.0,
-                    ),
-                  ),
-                ): ListView(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    ListView.builder(
-                      shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      itemCount: data.agentsModel.data.length,
-                      itemBuilder: (ctx, index) {
-                        return Column(
-                          children: <Widget>[
-                            ListTile(
-                              title: GestureDetector(
-                                onTap: () {
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute(
-                                      builder: (context) => TargetGraphSenior(),
-                                    ),
-                                  );
-                                },
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: <Widget>[
-                                    SizedBox(
-                                      width: MediaQuery.of(context).size.width * 0.23,
-                                      child: Text(
-                                        data.agentsModel.data[index].name,
-                                        softWrap: true,
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 2,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 10.0,
-                                    ),
-                                    Expanded(
-                                      child: LinearPercentIndicator(
+                    ? Center(
+                        child: Text(
+                          tr('extra.noTarget'),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                      )
+                    : ListView(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemCount: data.agentsModel.data.length,
+                            itemBuilder: (ctx, index) {
+                              return Column(
+                                children: <Widget>[
+                                  ListTile(
+                                    title: GestureDetector(
+                                      onTap: () {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                TargetGraphSenior(),
+                                          ),
+                                        );
+                                      },
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: <Widget>[
+                                          SizedBox(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width *
+                                                0.23,
+                                            child: Text(
+                                              data.agentsModel.data[index].name,
+                                              softWrap: true,
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 2,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 10.0,
+                                          ),
+                                          Expanded(
+                                            child: LinearPercentIndicator(
 //                              width: MediaQuery.of(context).size.width * 0.3,
-                                        animation: true,
-                                        animationDuration: 1000,
-                                        lineHeight: 25.0,
-                                        percent: data.agentsModel.data[index].analysis==null? 0.0:
-                                            double.parse(data.agentsModel.data[index].analysis.visited)/100,
-                                        center: Text("${data.agentsModel.data[index].analysis==null? 'No Target':
-                                        data.agentsModel.data[index].analysis.visited
-                                        }"),
-                                        linearStrokeCap: LinearStrokeCap.butt,
-                                        progressColor: Colors.red,
+                                              animation: true,
+                                              animationDuration: 1000,
+                                              lineHeight: 25.0,
+                                              percent: data
+                                                          .agentsModel
+                                                          .data[index]
+                                                          .analysis ==
+                                                      null
+                                                  ? 0.0
+                                                  : double.parse(data
+                                                          .agentsModel
+                                                          .data[index]
+                                                          .analysis
+                                                          .visited) /
+                                                      100,
+                                              center: Text(
+                                                  "${data.agentsModel.data[index].analysis == null ? 'No Target' : data.agentsModel.data[index].analysis.visited}"),
+                                              linearStrokeCap:
+                                                  LinearStrokeCap.butt,
+                                              progressColor: Colors.red,
+                                            ),
+                                          ),
+                                          Text(
+                                            "${data.agentsModel.data[index].analysis == null ? '' : data.agentsModel.data[index].analysis.visited}",
+                                            style: TextStyle(
+                                              fontSize: 12.0,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 10.0,
+                                          ),
+                                          Text(
+                                            tr('senior_profile.one_visit'),
+                                            style: TextStyle(
+                                              fontSize: 12.0,
+                                              color: Colors.blue,
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                     ),
-                                    Text(
-                                      "${data.agentsModel.data[index].analysis==null? '':data.agentsModel.data[index].analysis.visited}",
-                                      style: TextStyle(
-                                        fontSize: 12.0,
-                                        color: Colors.black,
+                                    leading: ClipRRect(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(10),
                                       ),
+                                      child: Image.asset('assets/user.png'),
                                     ),
-                                    SizedBox(
-                                      width: 10.0,
-                                    ),
-                                    Text(
-                                      tr('senior_profile.one_visit'),
-                                      style: TextStyle(
-                                        fontSize: 12.0,
-                                        color: Colors.blue,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              leading: ClipRRect(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(10),
-                                ),
-                                child: Image.asset('assets/user.png'),
-                              ),
-                            ),
-                            Divider(
-                              height: 2,
-                              indent: 0,
-                              endIndent: 50,
-                            )
-                          ],
-                        );
-                      },
-                    ),
-                  ],
-                );
+                                  ),
+                                  Divider(
+                                    height: 2,
+                                    indent: 0,
+                                    endIndent: 50,
+                                  )
+                                ],
+                              );
+                            },
+                          ),
+                        ],
+                      );
               });
             }
-          }
-      ),
+          }),
     );
   }
 }
